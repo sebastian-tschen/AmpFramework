163312a0380eec89223b3187de40368a375ed83a
frederic <frederic@e4dd7656-0c73-4daa-9ac4-3c17ed980afe>
2009-08-29 19:07:50 +0000

    Dynamisches Fenster (mit int anstatt double)

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -4,14 +4,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 public class MainFrame extends JFrame implements Runnable{
 	
 	private Window window;
 	private TheList list;
 	private int size;
+	private final int PIXEL = 128;
 		
 	public MainFrame(){
 		super("Häuptling Falsche Röhre");
@@ -20,12 +23,12 @@ public class MainFrame extends JFrame implements Runnable{
 		
 		createUI();
 		
-		window = new Window(128*size, 128*size);
+		window = new Window(PIXEL);
 		window.setSize(size);
 		
 		add(window, BorderLayout.CENTER);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(128*size+11, 128*size+30);
+		setSize(PIXEL*size+11, PIXEL*size+30);
 		setVisible(true);
 	}
 		
@@ -35,11 +38,32 @@ public class MainFrame extends JFrame implements Runnable{
 	
 	public void setKoords(TheList newList){
 		list = newList;
-		window.setList(list);
 	}
 
 	public void run() {
-		window.repaint();
+		while(true){
+			reSize(window.getSize());
+			window.setSize(size);
+			window.setList(list);
+			window.repaint();
+		}
+	}
+
+	private void reSize(Dimension bounds) {
+		int x = bounds.height;
+		int y = bounds.width;
+		double square;
+		if(x < y){
+			square = x;
+		}
+		else{
+			square = y;
+		}
+		size = (int)square / PIXEL;
+		if(size < 2){
+			size = 2;
+		}
+		System.err.println(size);
 	}
 
 }
--- src/gui/Window.java
+++ src/gui/Window.java
@@ -9,23 +9,21 @@ import javax.swing.Box.Filler;
 
 public class Window extends JPanel{
 
-	private int height;
-	private int width;
 	private TheList list;;
 	private int size;
+	private final int PIXEL;
 	
-	public Window(int newHeight, int newWidth){
-		height = newHeight;
-		width = newWidth;
+	public Window(int newPixel){
+		PIXEL = newPixel;
 		list = new TheList();
 	}
 	
 	public void paintComponent(Graphics gr){
 		Graphics2D gr2D = (Graphics2D) gr;
 		gr2D.setColor(Color.WHITE);
-		gr2D.fillRect(0, 0, width+2, height+2);
+		gr2D.fillRect(0, 0, size*PIXEL+2, size*PIXEL+2);
 		gr2D.setColor(Color.BLACK);
-		gr2D.drawOval(0, 0, width+2, height+2);
+		gr2D.drawOval(0, 0, size*PIXEL+2, size*PIXEL+2);
 		list.setSize(size);
 		list.paintAll(gr2D);
 	}
